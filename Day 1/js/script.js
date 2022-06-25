01d75f40f36f0e9726bc5b1628bc28b604d1099a
saba <sabera@alphaworks.tech>
2022-06-25 10:04:03 -0700

    practice variables

--- Day 1/js/script.js
+++ Day 1/js/script.js
@@ -1,32 +1,126 @@
- var displayElement =  document.getElementById("displayData");
-displayElement.innerHTML="Hello Students!!!"; // in line comment
 
-// CamelCase
-// snake_case
-/**
- * Describe what function does. Very good for documentation.
- */
-function getConfirmation(param){
-   var response=confirm(param);
-   var disp = `response ${response? 'OK':'Cancel'}  was clicked!`;
-   console.log(disp);
-}
+// var myVariable ="this is a string";
+// var myVariable2 ='as well';
+// console.log(myVariable.length );
+// console.log( myVariable2.length);
+// console.log( myVariable2.toUpperCase());
+// console.log( myVariable+' '+ myVariable2);
+//  console.log( `${ myVariable}
+//  dkfjkdfhg
+//  slh sd sd
+//  s;ldfh;sdlfh;dl
+//  ${ myVariable2}`);
+// console.log( '${ myVariable} ${ myVariable2}');
+// console.log("   'a'   ",'   "o"   ');
+// console.log('   \'single\'   ',"   \"double\"   ");
 
-console.log("Hi I'm console log");
-console.error("Hi I'm console error");
-console.warn("Hi I'm console warn");
+// var myNumber= 10;
+// console.log(myNumber);
 
-// Create a Github repository from command line (CLI)
-// https://gist.github.com/alexpchin/dc91e723d4db5018fef8
+// var myBoolean = true;
+// console.log(myBoolean);
+
+// myBoolean = false;
+// console.log(myBoolean);
+
+// var myUndefined;
+// console.log(myUndefined);
+// var myNull;
+// console.log(myNull);
+// myNull = null;
+// console.log(myNull);
+
+// var myBigInt = 1n;
+// console.log(myBigInt);
+// let Sym1 = Symbol("Sym");
+// let Sym2 = Symbol("Sym");
+
+// console.log(Sym1 === Sym2); // returns "false"
+// console.log(typeof myBoolean, myBoolean);
+// console.log(typeof myNumber, myNumber);
+// console.log(typeof myVariable, myVariable);
+// console.log(typeof myUndefined, myUndefined);
+// console.log(typeof myNull, myNull);
+
+// console.log( ` ${5+ myNumber}  ${ myBoolean}`);
+
+// var myVariable ="this is a string";
+// console.log(myVariable.replace("a","an") );
+// myVariable=myVariable.replace("a","an");
+// console.log(myVariable.split(''));
+// console.log(myVariable.split(' '));
+// console.log(myVariable.indexOf('an'));
+// console.log(myVariable.split(' ').re().join());
+
+// var otherVar= myVariable.split('').join();
+// console.log(otherVar);
+
+// var myNumber = 5;
+// var myFloat = 2.5;
+// console.log(myFloat+myNumber);
+
+
+// Solution for daily challenge
+// let myString = "Supercalifragilisticexpialidocious";
+// let anyNumber = 15;
+// let anyBigInt = 29572304598632049586340295374609n;
+// console.log("myString.length = ",myString.length);
+
+// console.log('anyNumber = ', anyNumber, `this is a ${typeof(anyNumber)}`);
+// console.log('anyBigInt = ', anyBigInt, `this is a ${typeof(anyBigInt)}`);
+// console.log(`anyBigInt = ${anyBigInt} this is a ${typeof(anyBigInt)}`);
+
+// var firstName;
+// //var last name; // Not Valid
+// var $LastName; // Valid
+// var _LastName; // Valid
+// // var 1LastName; // Not Valid
+
+// function example(){
+//     var name="Saba";
+// }
+// example();
+// console.log(name);
+
+// let sum=10;
+// // console.log(sum, ++sum,sum--,sum)
+// console.log(sum)
+// // ++sum
+// sum=sum+1
+// // sum+=1
+// console.log(sum)
+// // sum--
+// console.log(sum)
+// sum=sum-1
+// //sum-=1
+// console.log(sum)
+
+// let myVariable = 50;
+// myVariable=myVariable+1;
+// myVariable+=1;
+// ++myVariable;
+// myVariable++;
+// console.log(myVariable++);
+
+// console.log(myVariable);
+// myVariable="10";
+// console.log(typeof myVariable,myVariable)
+// console.log(+myVariable+5);
+// let otherVer = parseInt(myVariable)
+// console.log(typeof otherVer,otherVer )
+// var d = "23s5ring";
+// var e = parseInt(d);
+// console.log(e,+d);
+console.log(5+10*2);
+console.log((5+10)*2);
 
 
 
-/*
-Block of comments!
-We can comment in multiple lines.
 
-*/
 
-function IDontKnowWhat2Do(){
-   // TODO: need create new variable for ...
-}
+
+
+
+
+
+
